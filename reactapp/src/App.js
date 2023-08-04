70b550eea795e87bded77f9560ffff515ad97f91
Iamneo <techadmin@iamneo.ai>
2023-08-04 08:02:29 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Search from './components/Search/Search';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Search />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,7 +13,6 @@ function App() {
         >
           Learn React
         </a>
-      </header>
     </div>
   );
 }
